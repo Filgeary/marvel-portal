27262254fb5a246f4d8403b5919cebd155a25018
Georgiy Filippov <g.filippov777@gmail.com>
2023-07-06 18:30:45 +0500

    feat: add spinner while loading

--- src/containers/RandomCharContainer/RandomCharContainer.jsx
+++ src/containers/RandomCharContainer/RandomCharContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import CallToActionBox from '../../components/CallToActionBox'
 import RandomChar from '../../components/RandomChar'
+import Spinner from '../../components/Spinner'
 import { marvelService } from '../../services/marvelService'
 import { randomCharId } from '../../utils'
 import styles from './RandomCharContainer.module.css'
@@ -11,22 +12,30 @@ class RandomCharContainer extends React.Component {
      * @type {import('../../types/ICharacter').ICharacter | null}
      */
     char: null,
+    isLoading: false,
   }
 
   componentDidMount() {
+    this.handleUpdate()
+  }
+
+  handleUpdate() {
+    this.setState({ isLoading: true })
+
     marvelService
       .getAllChars({ limit: 1, offset: randomCharId() })
-      .then(res => this.setState({ char: res.data?.results?.at(0) }))
+      .then(res => this.setState({ char: res.data?.results?.at(0), isLoading: false }))
       .catch(err => console.error(err))
   }
 
   render() {
-    const { char } = this.state
+    const { char, isLoading } = this.state
 
     return (
       <div className='container'>
         <div className={styles.wrapper}>
-          <RandomChar char={char} />
+          {isLoading && <Spinner />}
+          {!isLoading && <RandomChar char={char} />}
           <CallToActionBox />
         </div>
       </div>
