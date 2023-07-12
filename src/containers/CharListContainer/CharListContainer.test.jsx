da08993a86e0be256fa39a03981edc1d1dc4f595
Georgiy Filippov <g.filippov777@gmail.com>
2023-07-12 08:32:20 +0500

    test: add tests for data fetching

--- src/containers/CharListContainer/CharListContainer.test.jsx
+++ src/containers/CharListContainer/CharListContainer.test.jsx
@@ -1,13 +1,42 @@
 import { render, screen } from '@testing-library/react'
 // import userEvent from '@testing-library/user-event'
+import { rest } from 'msw'
 import React from 'react'
+import { BASE_MARVEL_URL } from '../../constants'
+import { server } from '../../test/mocks/server'
 import CharListContainer from './CharListContainer'
 
-// TODO: make tests
-describe('CharListContainer', () => {
-  it('should render Heading & click on button', () => {
-    render(<CharListContainer />)
+const initRender = () => {
+  render(<CharListContainer />)
+}
 
-    expect(screen.getByRole('heading', { name: /Characters list/i })).toBeInTheDocument()
+describe('CharListContainer', () => {
+  it('should make initial fetch & render CharList', async () => {
+    initRender()
+
+    // initial spinner
+    expect(screen.getByTestId('spinner')).toBeInTheDocument()
+
+    // wait loading data
+    await screen.findByRole('heading', { name: /characters list/i })
+    expect(screen.getAllByTestId('charListItem')).toHaveLength(18)
+
+    // check for unmounting spinner
+    expect(screen.queryByTestId('spinner')).not.toBeInTheDocument()
+
+    // check for no error
+    expect(screen.queryByTestId('errorMessage')).not.toBeInTheDocument()
+  })
+
+  it('should handle known server errors', async () => {
+    server.use(
+      rest.get(`${BASE_MARVEL_URL}/characters`, (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({ message: 'Server Error' }))
+      }),
+    )
+    initRender()
+
+    await screen.findByTestId('errorMessage')
+    expect(screen.getByRole('heading', { name: /server error/i })).toBeInTheDocument()
   })
 })
