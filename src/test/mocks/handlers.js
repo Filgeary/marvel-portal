e12ca91d1fa8e906f07c1ade5b7c9e8697ac9c3a
Georgiy Filippov <g.filippov777@gmail.com>
2023-07-17 22:48:56 +0500

    feat: add new fixtures & update mocked handlers

--- src/test/mocks/handlers.js
+++ src/test/mocks/handlers.js
@@ -1,9 +1,16 @@
 import { rest } from 'msw'
 import allCharsJson from '../../__fixtures/api/allCharacters.json'
+import allCharsOffset315Json from '../../__fixtures/api/allCharactersOffset315.json'
 import { BASE_MARVEL_URL } from '../../constants'
 
 export const handlers = [
   rest.get(`${BASE_MARVEL_URL}/characters`, (req, res, ctx) => {
-    return res(ctx.json(allCharsJson))
+    const offset = req.url.searchParams.get('offset')
+
+    if (!offset || offset === '0') {
+      return res(ctx.json(allCharsJson))
+    } else {
+      return res(ctx.json(allCharsOffset315Json))
+    }
   }),
 ]
