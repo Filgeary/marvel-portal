4012e747dc0fcf518f9e88556ee2071a8542c5e2
Georgiy Filippov <g.filippov777@gmail.com>
2023-07-26 23:15:00 +0500

    feat: rewrite CharListContainer to hooks

--- src/containers/CharListContainer/CharListContainer.jsx
+++ src/containers/CharListContainer/CharListContainer.jsx
@@ -1,5 +1,5 @@
 import _uniqBy from 'lodash/uniqBy'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CharInfo from '../../components/CharInfo'
 import CharList from '../../components/CharList'
 import ErrorMessage from '../../components/_shared/ErrorMessage'
@@ -9,101 +9,80 @@ import { marvelService } from '../../services/marvelService'
 import { validateError } from '../../utils'
 import styles from './CharListContainer.module.css'
 
-class CharListContainer extends React.Component {
-  state = {
-    /**
-     * @type {import('../../types/ICharacter').ICharacter[] | null}
-     */
-    charList: null,
-    /**
-     * @type {import('../../types/ICharacter').ICharacter | null}
-     */
-    selectedChar: null,
-    isLoading: false,
-    isError: false,
-    errorMsg: '',
-    isInitialFetching: true,
+const CharListContainer = () => {
+  const [charList, setCharList] = useState(null) // TODO: add types
+  const [selectedChar, setSelectedChar] = useState(null) // TODO: add types
+  const [isLoading, setIsLoading] = useState(false)
+  const [isError, setIsError] = useState(false)
+  const [errorMsg, setErrorMsg] = useState('')
+  const [isInitialFetching, setIsInitialFetching] = useState(true)
+  const [paginationData, setPaginationData] = useState({
     offset: 0,
-    charsCount: 0,
-    totalChars: 0,
+    count: 0,
+    total: 0,
+  })
+
+  const handleError = err => {
+    setIsLoading(false)
+    setIsError(true)
+    setErrorMsg(validateError(err))
   }
 
-  componentDidMount() {
-    this.handleUpdate()
-  }
-
-  handleUpdate = (offset = 0, limit = CHAR_PAGE_LIMIT) => {
-    this.setState({ isLoading: true })
+  const handleUpdate = (offset = 0, limit = CHAR_PAGE_LIMIT) => {
+    setIsLoading(true)
 
     marvelService
       .getAllChars({ limit, offset })
       .then(res => {
         const { results = [], offset = 0, count = 0, total = 0 } = res.data ?? {}
 
-        this.setState(prevState => {
-          // to prevent duplicating in strict mode
-          const uniqedCharList = _uniqBy([...(prevState.charList ?? []), ...results], 'id')
-
-          return {
-            charList: uniqedCharList,
-            isLoading: false,
-            isError: false,
-            isInitialFetching: false,
-            offset: offset,
-            charsCount: count,
-            totalChars: total,
-          }
-        })
+        // to prevent duplicating in strict mode
+        // @ts-ignore
+        setCharList(prevState => _uniqBy([...(prevState ?? []), ...results], 'id'))
+        setIsLoading(false)
+        setIsError(false)
+        setIsInitialFetching(false)
+        setPaginationData({ offset, count, total })
       })
-      .catch(err => this.handleError(err))
+      .catch(err => handleError(err))
   }
 
-  handleError = err => {
-    this.setState({ isLoading: false, isError: true, errorMsg: validateError(err) })
+  // only run on mounting
+  useEffect(() => {
+    handleUpdate()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const handleSelectChar = id => {
+    // @ts-ignore
+    const selectedChar = charList?.find(char => char.id === id)
+    setSelectedChar(selectedChar)
   }
 
-  handleSelectChar = id => {
-    const selectedChar = this.state.charList?.find(char => char.id === id)
-    this.setState({ selectedChar })
-  }
+  const { offset, count, total } = paginationData
+  const hasMoreChars = total - offset - count > 0
 
-  render() {
-    const {
-      charList,
-      isLoading,
-      isError,
-      errorMsg,
-      selectedChar,
-      isInitialFetching,
-      offset,
-      charsCount,
-      totalChars,
-    } = this.state
-
-    const hasMoreChars = totalChars - offset - charsCount > 0
-
-    return (
-      <div className='container'>
-        <div className={styles.wrapper}>
-          <div style={{ minHeight: 250 }}>
-            {isLoading && isInitialFetching && <Spinner />}
-            {isError && <ErrorMessage text={errorMsg} />}
-            {!isError && !isInitialFetching && (
-              <CharList
-                charList={charList}
-                onSelectChar={id => this.handleSelectChar(id)}
-                onLoadMore={() => this.handleUpdate(offset + CHAR_PAGE_LIMIT)}
-                isLoading={isLoading}
-                hasMoreChars={hasMoreChars}
-              />
-            )}
-          </div>
-
-          <CharInfo char={selectedChar} />
+  return (
+    <div className='container'>
+      <div className={styles.wrapper}>
+        <div style={{ minHeight: 250 }}>
+          {isLoading && isInitialFetching && <Spinner />}
+          {isError && <ErrorMessage text={errorMsg} />}
+          {!isError && !isInitialFetching && (
+            <CharList
+              charList={charList}
+              onSelectChar={id => handleSelectChar(id)}
+              onLoadMore={() => handleUpdate(offset + CHAR_PAGE_LIMIT)}
+              isLoading={isLoading}
+              hasMoreChars={hasMoreChars}
+            />
+          )}
         </div>
+
+        <CharInfo char={selectedChar} />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default CharListContainer
