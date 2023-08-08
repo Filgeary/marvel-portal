89b1c85ff8c40776cb953248036652799ec009eb
Georgiy Filippov <g.filippov777@gmail.com>
2023-08-08 09:45:01 +0500

    style: fix ComicsBanner layout

--- src/components/ComicsBanner/ComicsBanner.jsx
+++ src/components/ComicsBanner/ComicsBanner.jsx
@@ -3,13 +3,11 @@ import styles from './ComicsBanner.module.css'
 
 const ComicsBanner = () => {
   return (
-    <div className='container'>
-      <section className={styles.wrapper}>
-        <h2 className={styles.heading}>
-          New comics every week! <br /> Stay tuned!
-        </h2>
-      </section>
-    </div>
+    <section className={styles.wrapper}>
+      <h2 className={styles.heading}>
+        New comics every week! <br /> Stay tuned!
+      </h2>
+    </section>
   )
 }
 
