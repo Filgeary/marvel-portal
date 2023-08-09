0e84e1f074d8fd5a04980cc22889675a99c41d78
Georgiy Filippov <g.filippov777@gmail.com>
2023-08-09 08:48:56 +0500

    test: add mock handler for comics

--- src/test/mocks/handlers.js
+++ src/test/mocks/handlers.js
@@ -1,6 +1,8 @@
 import { rest } from 'msw'
 import charactersJson from '../../__fixtures/api/characters.json'
 import charactersByOffset315Json from '../../__fixtures/api/charactersByOffset315.json'
+import comicsJson from '../../__fixtures/api/comics.json'
+import comicsByOffset147Json from '../../__fixtures/api/comicsByOffset147.json'
 import { BASE_MARVEL_URL } from '../../constants'
 
 export const handlers = [
@@ -11,6 +13,15 @@ export const handlers = [
       return res(ctx.json(charactersJson))
     } else {
       return res(ctx.json(charactersByOffset315Json))
+    }
+  }),
+  rest.get(`${BASE_MARVEL_URL}/comics`, (req, res, ctx) => {
+    const offset = req.url.searchParams.get('offset')
+
+    if (!offset || offset === '0') {
+      return res(ctx.json(comicsJson))
+    } else {
+      return res(ctx.json(comicsByOffset147Json))
     }
   }),
 ]
