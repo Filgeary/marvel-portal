714b540f16c0b378910b88450cc3935feefc6dc7
Georgiy Filippov <g.filippov777@gmail.com>
2023-08-10 11:45:33 +0500

    feat: add NotFound404

---
+++ src/components/_shared/NotFound404/NotFound404.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './NotFound404.module.css'
+import image404 from './assets/404-error.webp'
+
+const NotFound404 = () => {
+  return (
+    <section
+      data-testid='notFound404'
+      style={{ background: 'var(--cra-bg-light)' }}
+      className='d-flex flex-column gap-4 align-items-center text-center w-100'
+    >
+      <img
+        src={image404}
+        alt='404-error'
+        width={512}
+        height={512}
+      />
+      <h2 className={styles.heading}>Not Found Page!</h2>
+      <Link to='/'>Go to Home</Link>
+    </section>
+  )
+}
+
+export default NotFound404
