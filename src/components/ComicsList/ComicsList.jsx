5f36558b82fd85c6bce15073124fc700a018b3e7
Georgiy Filippov <g.filippov777@gmail.com>
2023-09-29 03:25:43 +0500

    feat: add animations to ComicsList

--- src/components/ComicsList/ComicsList.jsx
+++ src/components/ComicsList/ComicsList.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { createRef, useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { TransitionGroup } from 'react-transition-group'
 import { IMAGE_VARIANT } from '../../constants'
 import { transformComic } from '../../utils/apiAdapter'
+import { FadeInUp, FadeInZoomIn } from '../_shared/Animations'
 import styles from './ComicsList.module.css'
 
 /**
@@ -12,6 +14,14 @@ import styles from './ComicsList.module.css'
  * @param {boolean} props.hasMoreComics
  */
 const ComicsList = ({ comicsList, onLoadMore, isLoading, hasMoreComics }) => {
+  const listRef = useRef(null)
+  const [isShowList, setIsShowList] = useState(false)
+  // for animations
+  useEffect(() => {
+    setIsShowList(Boolean(comicsList?.length))
+    return () => setIsShowList(false)
+  }, [comicsList?.length])
+
   const transformedComicsList = comicsList?.map(comic =>
     transformComic(comic, IMAGE_VARIANT['216x324']),
   )
@@ -28,41 +38,60 @@ const ComicsList = ({ comicsList, onLoadMore, isLoading, hasMoreComics }) => {
     <section className={styles.section}>
       <h2 className='visually-hidden'>Comics List</h2>
 
-      <ul className={styles.list}>
-        {transformedComicsList.map(comic => {
-          const { id, title, thumbnail, onsaleDate, printPrice } = comic ?? {}
+      {/* @ts-ignore */}
+      <FadeInZoomIn
+        in={isShowList}
+        ref={listRef}
+      >
+        <ul
+          data-testid='comicsListUList'
+          className={styles.list}
+          ref={listRef}
+        >
+          <TransitionGroup component={null}>
+            {transformedComicsList.map(comic => {
+              const { id, title, thumbnail, onsaleDate, printPrice } = comic ?? {}
+              const liRef = createRef()
 
-          return (
-            <li
-              key={id}
-              data-testid='comicsListItem'
-              className={styles.listItem}
-            >
-              <Link
-                to={`/comics/${id}`}
-                className={styles.listLink}
-              >
-                <figure className='overflow-hidden'>
-                  <img
-                    data-testid='comicsListItemImage'
-                    loading='lazy'
-                    src={thumbnail}
-                    alt={title}
-                    width={216}
-                    height={324}
-                  />
-                </figure>
+              return (
+                /* @ts-ignore */
+                <FadeInUp
+                  key={id}
+                  ref={liRef}
+                >
+                  <li
+                    ref={liRef}
+                    data-testid='comicsListItem'
+                    className={styles.listItem}
+                  >
+                    <Link
+                      to={`/comics/${id}`}
+                      className={styles.listLink}
+                    >
+                      <figure className='overflow-hidden'>
+                        <img
+                          data-testid='comicsListItemImage'
+                          loading='lazy'
+                          src={thumbnail}
+                          alt={title}
+                          width={216}
+                          height={324}
+                        />
+                      </figure>
 
-                <div className='d-flex flex-column text-center p-1 px-05'>
-                  <h3 className={styles.title}>{title}</h3>
-                  <small className={styles.onsaleDate}>{onsaleDate}</small>
-                  <small className={styles.printPrice}>{printPrice}</small>
-                </div>
-              </Link>
-            </li>
-          )
-        })}
-      </ul>
+                      <div className='d-flex flex-column text-center p-1 px-05'>
+                        <h3 className={styles.title}>{title}</h3>
+                        <small className={styles.onsaleDate}>{onsaleDate}</small>
+                        <small className={styles.printPrice}>{printPrice}</small>
+                      </div>
+                    </Link>
+                  </li>
+                </FadeInUp>
+              )
+            })}
+          </TransitionGroup>
+        </ul>
+      </FadeInZoomIn>
 
       {hasMoreComics ? (
         <button
